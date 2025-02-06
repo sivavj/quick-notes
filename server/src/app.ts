c3023c9ba977c6ba7a83ce52a74cccfb49b68274
Siva <sivavj002@gmail.com>
2025-02-06 15:36:56 +0530

    add default route

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/", async (req, res) => {
+  res.send("Welcome to Quick Notes API");
+});
 app.use("/api/notes", notesRoutes);
 
 export default app;
